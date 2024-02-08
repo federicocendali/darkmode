714bad038b383189a4cecd0beeee2548b7cbda4a
Federico Cendali <federicocendali@gmail.com>
2024-02-08 02:49:56 -0300

    [feature] - finished project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
-let navBar = document.querySelector('.navbar');
-let darkMode = document.querySelector('.dark-mode-icon');
-let body = document.body;
+document.addEventListener('DOMContentLoaded', function () {
+  let navBar = document.querySelector('.navbar');
+  let darkMode = document.querySelector('.dark-mode-icon');
+  let body = document.body;
+  let darkModeState = localStorage.getItem('darkModeState');
 
-darkMode.addEventListener('click', () => {
-  navBar.classList.toggle('dark-nav');
-  body.classList.toggle('dark-mode');
+  // Check if dark mode is enabled
+  if (darkModeState === 'enabled') {
+    enableDarkMode();
+  }
+
+  // Toggle dark mode
+  darkMode.addEventListener('click', () => {
+    darkModeState = localStorage.getItem('darkModeState');
+    if (darkModeState !== 'enabled') {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
+
+  // Enable dark mode
+  function enableDarkMode() {
+    navBar.classList.add('dark-nav');
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkModeState', 'enabled');
+  }
+
+  // Disable dark mode
+  function disableDarkMode() {
+    navBar.classList.remove('dark-nav');
+    body.classList.remove('dark-mode');
+    localStorage.setItem('darkModeState', null);
+  }
 });
